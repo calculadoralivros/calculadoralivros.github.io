e39032b909cf7185ea862e3fbcfd908aa849d331
calculadoralivros <157849144+calculadoralivros@users.noreply.github.com>
2025-01-31 18:04:48 +0000

    Update script.js
    Atualização preços CTT 2025

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ function calculate() {
             var inputValue = parseFloat(document.getElementById("inputValue").value);
 
             if (!isNaN(inputValue) && inputValue >= 501 && inputValue <= 2000) {
-                var result = 1.451 + (0.599 * inputValue) / 1000;
+                var result = 1.509 + (0.623 * inputValue) / 1000;
                 document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";
             } else if (!isNaN(inputValue) && inputValue >= 251 && inputValue <= 500) {
-                var result = 0.133 + (2.581 * inputValue) / 1000;
+                var result = 0.139 + (2.684 * inputValue) / 1000;
                 document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";
             } else if (!isNaN(inputValue) && inputValue >= 101 && inputValue <= 250) {
-                var result = 0.183 + (2.052 * inputValue) / 1000;
+                var result = 0.191 + (2.134 * inputValue) / 1000;
                 document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";
             } else if (!isNaN(inputValue) && inputValue <= 100) {
-                var result = 0.282 + (1.070 * inputValue) / 1000;
+                var result = 0.294 + (1.113 * inputValue) / 1000;
                 document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";	
             } else if (!isNaN(inputValue) && inputValue > 2000 && inputValue <= 5000) {
                 // Round down to the nearest thousand
                 inputValue = Math.floor(inputValue / 1000);
-                var result2000 = 1.451 + (0.599 * 2000) / 1000;
-                var resultAbove2000 = result2000 + ((inputValue-1) * 1.20);
+                var result2000 = 1.509 + (0.623 * 2000) / 1000;
+                var resultAbove2000 = result2000 + ((inputValue-1) * 1.28);
                 var finalResultAbove2000 = resultAbove2000 * 1.23; // Adding 23% tax only for values above 2000
                 document.getElementById("custo").innerText = "Custo: " + roundToDecimal(finalResultAbove2000, 2) + " € com IVA";
             } else {
