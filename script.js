42e081b13a7590d0539f4ae25fb12d88448b9c47
calculadoralivros <157849144+calculadoralivros@users.noreply.github.com>
2025-02-07 20:05:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,18 @@ function calculate() {
         var result = 1.509 + (0.623 * inputValue) / 1000;
         document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";
         document.getElementById("custoAzoresMadeira").innerText = ""; // Clear secondary cost
-        document.getElementById("custo").style.fontWeight = "normal"; // Reset font-weight
     } else if (!isNaN(inputValue) && inputValue >= 251 && inputValue <= 500) {
         var result = 0.139 + (2.684 * inputValue) / 1000;
         document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";
         document.getElementById("custoAzoresMadeira").innerText = "";
-        document.getElementById("custo").style.fontWeight = "normal"; // Reset font-weight
     } else if (!isNaN(inputValue) && inputValue >= 101 && inputValue <= 250) {
         var result = 0.191 + (2.134 * inputValue) / 1000;
         document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";
         document.getElementById("custoAzoresMadeira").innerText = "";
-        document.getElementById("custo").style.fontWeight = "normal"; // Reset font-weight
     } else if (!isNaN(inputValue) && inputValue <= 100) {
         var result = 0.294 + (1.113 * inputValue) / 1000;
         document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";    
         document.getElementById("custoAzoresMadeira").innerText = "";
-        document.getElementById("custo").style.fontWeight = "normal"; // Reset font-weight
     } else if (!isNaN(inputValue) && inputValue > 2000 && inputValue <= 5000) {
         // Round down to the nearest thousand
         inputValue = Math.floor(inputValue / 1000);
@@ -33,15 +29,12 @@ function calculate() {
 
         document.getElementById("custo").innerText = 
             "Custo: " + roundToDecimal(finalResultAbove2000, 2) + " € com IVA (Portugal Continental)";
+        
         document.getElementById("custoAzoresMadeira").innerText = 
             "Custo: " + roundToDecimal(finalResultAzoresMadeira, 2) + " € com IVA (Envio para Ilhas Açores e Madeira)";
-        
-        // Make the Custo bold for input values above 2000
-        document.getElementById("custo").style.fontWeight = "bold";
     } else {
         document.getElementById("custo").innerText = "Valor inválido. Adicione um valor entre 0 a 5000 gramas.";
         document.getElementById("custoAzoresMadeira").innerText = "";
-        document.getElementById("custo").style.fontWeight = "normal"; // Reset font-weight
     }
 }
 
