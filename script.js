ca9412251f1a4081903cfd1fc3b21b2752650667
calculadoralivros <157849144+calculadoralivros@users.noreply.github.com>
2025-04-01 11:22:38 +0100

    Update script.js
    Preco CTT ajustado

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function calculate() {
     var inputValue = parseFloat(document.getElementById("inputValue").value);
 
-    if (!isNaN(inputValue) && inputValue >= 501 && inputValue <= 2000) {
-        var result = 1.509 + (0.623 * inputValue) / 1000;
-        document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";
-        document.getElementById("custoAzoresMadeira").innerText = ""; // Clear secondary cost
-        document.getElementById("custo").style.fontWeight = "normal"; // Reset font-weight
-    } else if (!isNaN(inputValue) && inputValue >= 251 && inputValue <= 500) {
-        var result = 0.139 + (2.684 * inputValue) / 1000;
-        document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";
+    if (!isNaN(inputValue) && inputValue <= 100) {
+        var result = 0.296 + (1.120 * inputValue) / 1000;
+        document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";    
         document.getElementById("custoAzoresMadeira").innerText = "";
         document.getElementById("custo").style.fontWeight = "normal"; // Reset font-weight
     } else if (!isNaN(inputValue) && inputValue >= 101 && inputValue <= 250) {
-        var result = 0.191 + (2.134 * inputValue) / 1000;
+        var result = 0.192 + (2.151 * inputValue) / 1000;
         document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";
         document.getElementById("custoAzoresMadeira").innerText = "";
         document.getElementById("custo").style.fontWeight = "normal"; // Reset font-weight
-    } else if (!isNaN(inputValue) && inputValue <= 100) {
-        var result = 0.294 + (1.113 * inputValue) / 1000;
-        document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";    
+    } else if (!isNaN(inputValue) && inputValue >= 251 && inputValue <= 500) {
+        var result = 0.140 + (2.706 * inputValue) / 1000;
+        document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";
         document.getElementById("custoAzoresMadeira").innerText = "";
+        document.getElementById("custo").style.fontWeight = "normal"; // Reset font-weight
+    } else if (!isNaN(inputValue) && inputValue >= 501 && inputValue <= 2000) {
+        var result = 1.519 + (0.625 * inputValue) / 1000;
+        document.getElementById("custo").innerText = "Custo: " + roundToDecimal(result, 2) + " €";
+        document.getElementById("custoAzoresMadeira").innerText = ""; // Clear secondary cost
         document.getElementById("custo").style.fontWeight = "normal"; // Reset font-weight
     } else if (!isNaN(inputValue) && inputValue > 2000 && inputValue <= 5000) {
         // Round down to the nearest thousand
         inputValue = Math.floor(inputValue / 1000);
-        var result2000 = 1.509 + (0.623 * 2000) / 1000;
+        var result2000 = 1.519 + (0.625 * 2000) / 1000;
         var resultAbove2000 = result2000 + ((inputValue - 1) * 1.28);
         var finalResultAbove2000 = resultAbove2000 * 1.23; // Applying 23% VAT
 
